9de504eb513f6fe8571f377490fc77a44e28bc8d
Anatol V <malfenius@gmail.com>
2020-12-26 15:19:13 +0300

    Выведение типов

---
+++ java8/src/test/java/ru/avishnyakov/javaex/TypeInferenceTest.java
@@ -0,0 +1,44 @@
+package ru.avishnyakov.javaex;
+
+import org.junit.jupiter.api.Test;
+
+import java.awt.*;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.BinaryOperator;
+import java.util.function.Predicate;
+
+public class TypeInferenceTest {
+    private void useHashMap(Map<String, String> map) {
+        // code
+    }
+
+    @Test
+    public void testDiamond() {
+        Map<String, String> oldWordCounts = new HashMap<String, String>();
+        Map<Object, Object> diamondWordCounts = new HashMap<>();
+
+        // в Java 7 не откомпилируется
+        useHashMap(new HashMap<>());
+
+        BinaryOperator add = (o, o2) -> o;
+
+        final Button button = new Button();
+        button.addActionListener(event -> System.out.println(event.getActionCommand()));
+
+        // два метода check с одинаковой сигнатурой
+//        check(value -> value > 0);
+    }
+
+    interface IntPred {
+        boolean test(Integer value);
+    }
+
+    boolean check(Predicate<Integer> predicate) {
+        return true;
+    }
+
+    boolean check(IntPred predicate) {
+        return true;
+    }
+}
