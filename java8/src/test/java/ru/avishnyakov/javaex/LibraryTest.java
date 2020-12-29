d164a9856a16a7f0fa757fe35014791d7e72319b
Anatol V <malfenius@gmail.com>
2020-12-29 15:11:50 +0300

    Перегрузка

--- java8/src/test/java/ru/avishnyakov/javaex/LibraryTest.java
+++ java8/src/test/java/ru/avishnyakov/javaex/LibraryTest.java
@@ -6,6 +6,8 @@ import org.slf4j.LoggerFactory;
 import ru.avishnyakov.javaex.model.Track;
 
 import java.util.LongSummaryStatistics;
+import java.util.function.BinaryOperator;
+import java.util.function.Predicate;
 import java.util.function.Supplier;
 
 import static ru.avishnyakov.javaex.TestData.album;
@@ -38,5 +40,60 @@ public class LibraryTest {
                 .summaryStatistics();
 
         System.out.println(statistics);
+    }
+
+    @Test
+    public void testOverloadByType() {
+        overloadMethod("abc");
+    }
+
+    private void overloadMethod(Object o) {
+        System.out.println("Object");
+    }
+
+    private void overloadMethod(String s) {
+        System.out.println("String");
+    }
+
+    @Test
+    public void testOverloadByLambda() {
+        overloadMethodLambda((x, y) -> x + y);
+    }
+
+    private void overloadMethodLambda(BinaryOperator<Integer> lambda) {
+        System.out.println("BinaryOperator");
+    }
+
+    private void overloadMethodLambda(IntegerBiFunction lambda) {
+        System.out.println("IntegerBiFunction");
+    }
+
+    // throwing exception
+//    private void overloadMethodLambda(StringBiFunction lambda) {
+//        System.out.println("StringBiFunction");
+//    }
+
+    private interface IntegerBiFunction extends BinaryOperator<Integer> {
+    }
+
+    private interface StringBiFunction extends BinaryOperator<String> {
+    }
+
+    @Test
+    public void testOverloadPredicate() {
+        // throwing exception
+//        overloadMethodPredicate((x) -> true);
+    }
+
+    private interface IntPredicate {
+        boolean test(int value);
+    }
+
+    private void overloadMethodPredicate(Predicate<Integer> predicate) {
+        System.out.println("Predicate");
+    }
+
+    private void overloadMethodPredicate(IntPredicate predicate) {
+        System.out.println("IntPredicate");
     }
 }
