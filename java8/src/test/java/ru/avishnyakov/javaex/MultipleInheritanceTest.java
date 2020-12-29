eaeaa51924ae16913ab4763c85b1c4d048c858eb
Anatol V <malfenius@gmail.com>
2020-12-29 17:19:26 +0300

    Множественное наследование

---
+++ java8/src/test/java/ru/avishnyakov/javaex/MultipleInheritanceTest.java
@@ -0,0 +1,35 @@
+package ru.avishnyakov.javaex;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+interface Jukebox {
+    default String rock() {
+        return "... all over the world!";
+    }
+}
+
+interface Carriage {
+    default String rock() {
+        return "... from side to side";
+    }
+}
+
+class MusicalCarriage implements Carriage, Jukebox {
+    @Override
+    public String rock() {
+        // необходимо явно указать
+        // на метод унаследованного
+        // интерфейса
+        return Carriage.super.rock();
+    }
+}
+
+public class MultipleInheritanceTest {
+    @Test
+    public void testEqualDefaultMethod() {
+        final MusicalCarriage carriage = new MusicalCarriage();
+        assertEquals("... from side to side", carriage.rock());
+    }
+}
