6f1801b6f98c279776973e7e9900550295b06335
Anatol V <malfenius@gmail.com>
2020-12-29 14:37:03 +0300

    Примитивы

--- java8/src/test/java/ru/avishnyakov/javaex/LibraryTest.java
+++ java8/src/test/java/ru/avishnyakov/javaex/LibraryTest.java
@@ -3,8 +3,12 @@ package ru.avishnyakov.javaex;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import ru.avishnyakov.javaex.model.Track;
 
+import java.util.LongSummaryStatistics;
 import java.util.function.Supplier;
+
+import static ru.avishnyakov.javaex.TestData.album;
 
 public class LibraryTest {
     private static final Logger logger = LoggerFactory.getLogger(LibraryTest.class);
@@ -25,5 +29,14 @@ public class LibraryTest {
         if (logger.isDebugEnabled()) {
             logger.debug(message.get());
         }
+    }
+
+    @Test
+    public void testStatistics() {
+        final LongSummaryStatistics statistics = album.getTracks()
+                .mapToLong(Track::getLength)
+                .summaryStatistics();
+
+        System.out.println(statistics);
     }
 }
