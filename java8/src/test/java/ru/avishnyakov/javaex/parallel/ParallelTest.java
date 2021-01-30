32b204de72ccb31c17d7abe64e111f94d98fc48c
Anatol V <malfenius@gmail.com>
2021-01-30 22:35:32 +0300

    Параллельные операции с массивами

--- java8/src/test/java/ru/avishnyakov/javaex/parallel/ParallelTest.java
+++ java8/src/test/java/ru/avishnyakov/javaex/parallel/ParallelTest.java
@@ -1,7 +1,9 @@
 package ru.avishnyakov.javaex.parallel;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
 import java.util.Map;
 import java.util.Random;
 import java.util.function.IntFunction;
@@ -28,5 +30,48 @@ public class ParallelTest {
 
     private IntFunction<Integer> twoDiceThrows() {
         return value -> new Random().nextInt(6) + new Random().nextInt(6);
+    }
+
+    @Test
+    public void testParallelSetAllInitialize() {
+        final double[] values = parallelInitialize(10);
+        final double[] values2 = imperativeInitialize(10);
+
+        Assertions.assertArrayEquals(values, values2);
+    }
+
+    public static double[] imperativeInitialize(int size) {
+        final double[] values = new double[size];
+        for (int i = 0; i < values.length; i++) {
+            values[i] = i;
+        }
+        return values;
+    }
+
+    public static double[] parallelInitialize(int size) {
+        final double[] values = new double[size];
+        Arrays.parallelSetAll(values, i -> i);
+        return values;
+    }
+
+    @Test
+    public void testParallelPrefix() {
+        final double[] values = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
+        final double[] result = simpleMovingAverage(values, 2);
+        System.out.println(Arrays.toString(result));
+    }
+
+    public static double[] simpleMovingAverage(double[] values, int n) {
+        final double[] sums = Arrays.copyOf(values, values.length);
+        Arrays.parallelPrefix(sums, Double::sum);
+        System.out.println("Sum: " + Arrays.toString(sums));
+
+        int start = n - 1;
+        return IntStream.range(start, sums.length)
+                .mapToDouble(i -> {
+                    double prefix = i == start ? 0 : sums[i - n];
+                    return (sums[i] - prefix) / n;
+                })
+                .toArray();
     }
 }
