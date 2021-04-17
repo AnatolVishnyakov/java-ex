2901c23247c65449a0545b1ba34325d8ce515324
Anatol V <malfenius@gmail.com>
2021-04-17 20:08:46 +0300

    Изучение DateTime API

---
+++ java8/src/test/java/ru/avishnyakov/javaex/datetime/JavaUtilDateTest.java
@@ -0,0 +1,44 @@
+package ru.avishnyakov.javaex.datetime;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.Instant;
+import java.util.Date;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class JavaUtilDateTest {
+    private static final Date pastDate = new Date(1518678377185L);
+    private static final Date futureDate = new Date(4718678377185L);
+    private static final Date currentDate = new Date(1618678865548L);
+
+    @Test
+    public void testAfterDate() {
+        assertTrue(currentDate.after(pastDate));
+        assertFalse(currentDate.after(futureDate));
+    }
+
+    @Test
+    public void testBeforeDate() {
+        assertTrue(currentDate.before(futureDate));
+        assertFalse(currentDate.before(pastDate));
+    }
+
+    @Test
+    public void testEqualsDate() {
+        assertEquals(currentDate, new Date(currentDate.getTime()));
+    }
+
+    @Test
+    public void testCompareToDate() {
+        assertEquals(1, currentDate.compareTo(pastDate));
+        assertEquals(-1, currentDate.compareTo(futureDate));
+        assertEquals(0, currentDate.compareTo(new Date(currentDate.getTime())));
+    }
+
+    @Test
+    public void testInstantDate() {
+        assertNotNull(currentDate.toInstant());
+        assertEquals(Instant.class, currentDate.toInstant().getClass());
+    }
+}
