ce9acfe2161dfc6126e7f16b2aa41d498a134d4f
Anatol V <malfenius@gmail.com>
2021-05-10 22:42:35 +0300

    Изучение DateTime API. Instant

--- java8/src/test/java/ru/avishnyakov/javaex/datetime/InstantTest.java
+++ java8/src/test/java/ru/avishnyakov/javaex/datetime/InstantTest.java
@@ -3,11 +3,15 @@ package ru.avishnyakov.javaex.datetime;
 import org.junit.jupiter.api.Test;
 
 import java.time.Instant;
+import java.time.ZoneId;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class InstantTest {
+    private static final Instant instant = Instant.ofEpochMilli(1618678865548L);
+
     @Test
-    public void test() {
-        Instant now = Instant.now();
-        System.out.println(now);
+    public void testAtZone() {
+        assertEquals(ZoneId.of("Europe/Moscow"), instant.atZone(ZoneId.of("Europe/Moscow")).getZone());
     }
 }
