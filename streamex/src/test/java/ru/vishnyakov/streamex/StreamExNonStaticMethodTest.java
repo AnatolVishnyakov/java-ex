e47fd9b427142a23257e559292cd850f9ae968c6
Anatol V <malfenius@gmail.com>
2021-06-25 23:19:29 +0300

    Изучение StreamEx API.

---
+++ streamex/src/test/java/ru/vishnyakov/streamex/StreamExNonStaticMethodTest.java
@@ -0,0 +1,550 @@
+package ru.vishnyakov.streamex;
+
+import org.junit.jupiter.api.Test;
+
+public class StreamExNonStaticMethodTest {
+    @Test
+    void allMatch() {
+
+    }
+
+    @Test
+    void anyMatch() {
+
+    }
+
+    @Test
+    void append() {
+
+    }
+
+    @Test
+    void chain() {
+
+    }
+
+    @Test
+    void close() {
+
+    }
+
+    @Test
+    void collapse() {
+
+    }
+
+    @Test
+    void collect() {
+
+    }
+
+    @Test
+    void count() {
+
+    }
+
+    @Test
+    void cross() {
+
+    }
+
+    @Test
+    void distinct() {
+
+    }
+
+    @Test
+    void dropWhile() {
+
+    }
+
+    @Test
+    void equals() {
+
+    }
+
+    @Test
+    void filter() {
+
+    }
+
+    @Test
+    void filterBy() {
+
+    }
+
+    @Test
+    void findAny() {
+
+    }
+
+    @Test
+    void findFirst() {
+
+    }
+
+    @Test
+    void flatArray() {
+
+    }
+
+    @Test
+    void flatCollection() {
+
+    }
+
+    @Test
+    void flatMap() {
+
+    }
+
+    @Test
+    void flatMapToDouble() {
+
+    }
+
+    @Test
+    void flatMapToEntry() {
+
+    }
+
+    @Test
+    void flatMapToInt() {
+
+    }
+
+    @Test
+    void flatMapToLong() {
+
+    }
+
+    @Test
+    void foldLeft() {
+
+    }
+
+    @Test
+    void foldRight() {
+
+    }
+
+    @Test
+    void forEach() {
+
+    }
+
+    @Test
+    void forEachOrdered() {
+
+    }
+
+    @Test
+    void forPairs() {
+
+    }
+
+    @Test
+    void groupRuns() {
+
+    }
+
+    @Test
+    void groupingBy() {
+
+    }
+
+    @Test
+    void groupingTo() {
+
+    }
+
+    @Test
+    void has() {
+
+    }
+
+    @Test
+    void headTail() {
+
+    }
+
+    @Test
+    void ifEmpty() {
+
+    }
+
+    @Test
+    void indexOf() {
+
+    }
+
+    @Test
+    void intersperse() {
+
+    }
+
+    @Test
+    void intervalMap() {
+
+    }
+
+    @Test
+    void into() {
+
+    }
+
+    @Test
+    void isParallel() {
+
+    }
+
+    @Test
+    void iterator() {
+
+    }
+
+    @Test
+    void joining() {
+
+    }
+
+    @Test
+    void limit() {
+
+    }
+
+    @Test
+    void map() {
+
+    }
+
+    @Test
+    void mapFirst() {
+
+    }
+
+    @Test
+    void mapFirstOrElse() {
+
+    }
+
+    @Test
+    void mapLast() {
+
+    }
+
+    @Test
+    void mapLastOrElse() {
+
+    }
+
+    @Test
+    void mapPartial() {
+
+    }
+
+    @Test
+    void mapToDouble() {
+
+    }
+
+    @Test
+    void mapToEntry() {
+
+    }
+
+    @Test
+    void mapToInt() {
+
+    }
+
+    @Test
+    void mapToLong() {
+
+    }
+
+    @Test
+    void max() {
+
+    }
+
+    @Test
+    void maxBy() {
+
+    }
+
+    @Test
+    void maxByDouble() {
+
+    }
+
+    @Test
+    void maxByInt() {
+
+    }
+
+    @Test
+    void maxByLong() {
+
+    }
+
+    @Test
+    void min() {
+
+    }
+
+    @Test
+    void minBy() {
+
+    }
+
+    @Test
+    void minByDouble() {
+
+    }
+
+    @Test
+    void minByInt() {
+
+    }
+
+    @Test
+    void minByLong() {
+
+    }
+
+    @Test
+    void nonNull() {
+
+    }
+
+    @Test
+    void noneMatch() {
+
+    }
+
+    @Test
+    void onClose() {
+
+    }
+
+    @Test
+    void pairMap() {
+
+    }
+
+    @Test
+    void parallel() {
+
+    }
+
+    @Test
+    void partitioningBy() {
+
+    }
+
+    @Test
+    void partitioningTo() {
+
+    }
+
+    @Test
+    void peek() {
+
+    }
+
+    @Test
+    void peekFirst() {
+
+    }
+
+    @Test
+    void peekLast() {
+
+    }
+
+    @Test
+    void prefix() {
+
+    }
+
+    @Test
+    void prepend() {
+
+    }
+
+    @Test
+    void reduce() {
+
+    }
+
+    @Test
+    void remove() {
+
+    }
+
+    @Test
+    void removeBy() {
+
+    }
+
+    @Test
+    void reverseSorted() {
+
+    }
+
+    @Test
+    void runLengths() {
+
+    }
+
+    @Test
+    void scanLeft() {
+
+    }
+
+    @Test
+    void scanRight() {
+
+    }
+
+    @Test
+    void select() {
+
+    }
+
+    @Test
+    void sequential() {
+
+    }
+
+    @Test
+    void skip() {
+
+    }
+
+    @Test
+    void sorted() {
+
+    }
+
+    @Test
+    void sortedBy() {
+
+    }
+
+    @Test
+    void sortedByDouble() {
+
+    }
+
+    @Test
+    void sortedByInt() {
+
+    }
+
+    @Test
+    void sortedByLong() {
+
+    }
+
+    @Test
+    void spliterator() {
+
+    }
+
+    @Test
+    void takeWhile() {
+
+    }
+
+    @Test
+    void takeWhileInclusive() {
+
+    }
+
+    @Test
+    void toArray() {
+
+    }
+
+    @Test
+    void toCollection() {
+
+    }
+
+    @Test
+    void toFlatCollection() {
+
+    }
+
+    @Test
+    void toFlatList() {
+
+    }
+
+    @Test
+    void toImmutableList() {
+
+    }
+
+    @Test
+    void toImmutableSet() {
+
+    }
+
+    @Test
+    void toList() {
+
+    }
+
+    @Test
+    void toListAndThen() {
+
+    }
+
+    @Test
+    void toMap() {
+
+    }
+
+    @Test
+    void toNavigableMap() {
+
+    }
+
+    @Test
+    void toSet() {
+
+    }
+
+    @Test
+    void toSetAndThen() {
+
+    }
+
+    @Test
+    void toSortedMap() {
+
+    }
+
+    @Test
+    void unordered() {
+
+    }
+
+    @Test
+    void withFirst() {
+
+    }
+
+    @Test
+    void without() {
+
+    }
+
+    @Test
+    void zipWith() {
+
+    }
+}
---
+++ streamex/src/test/java/ru/vishnyakov/streamex/StreamExStaticMethodTest.java
@@ -0,0 +1,129 @@
+package ru.vishnyakov.streamex;
+
+import one.util.streamex.StreamEx;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.TreeSet;
+import java.util.stream.Collectors;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class StreamExStaticMethodTest {
+
+    @Test
+    @DisplayName("Сочетания")
+    void cartesianPower() {
+        assertEquals("[[1, 1], [1, 2], [2, 1], [2, 2]]", StreamEx.cartesianPower(2, Arrays.asList(1, 2)).toList().toString());
+//        StreamEx.cartesianPower(2, Arrays.asList(1, 3), Integer.class, Integer::sum)
+    }
+
+    @Test
+    void cartesianProduct() {
+
+    }
+
+    @Test
+    void constant() {
+
+    }
+
+    @Test
+    void empty() {
+
+    }
+
+    @Test
+    void generate() {
+
+    }
+
+    @Test
+    void iterate() {
+
+    }
+
+    @Test
+    void of() {
+
+    }
+
+    @Test
+    void ofCombinations() {
+
+    }
+
+    @Test
+    void ofKeys() {
+
+    }
+
+    @Test
+    void ofLines() {
+
+    }
+
+    @Test
+    void ofNullable() {
+
+    }
+
+    @Test
+    void ofPairs() {
+
+    }
+
+    @Test
+    void ofPermutations() {
+
+    }
+
+    @Test
+    void ofReversed() {
+
+    }
+
+    @Test
+    void ofSubLists() {
+
+    }
+
+    @Test
+    void ofTree() {
+
+    }
+
+    @Test
+    void ofValues() {
+
+    }
+
+    @Test
+    void produce() {
+
+    }
+
+    @Test
+    void split() {
+
+    }
+
+    @Test
+    void zip() {
+
+    }
+
+    @Test
+    void f() {
+        Arrays.stream(StreamEx.class.getMethods())
+                .filter(method -> (method.getModifiers() & Modifier.STATIC) == 0)
+                .map(Method::getName)
+                .sorted(Comparator.naturalOrder())
+                .collect(Collectors.toCollection(() -> new TreeSet<String>(Comparator.naturalOrder())))
+                .forEach(System.out::println);
+    }
+}
