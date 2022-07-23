1c65b876fae5d5ee148ed9685c421dded33314bb
Anatol V <malfenius@gmail.com>
2022-07-23 22:23:55 +0300

    Изучение Netty.

---
+++ nettyex/src/main/java/ru/avish/nettyex/ResponseData.java
@@ -0,0 +1,13 @@
+package ru.avish.nettyex;
+
+public class ResponseData {
+    private int intValue;
+
+    public int getIntValue() {
+        return intValue;
+    }
+
+    public void setIntValue(int intValue) {
+        this.intValue = intValue;
+    }
+}
